ba84cf95c5c363054b178f75f10873a438d55107
andrew <asc269@cornell.edu>
2023-03-05 00:41:46 -0500

    Add getLocalStream() - gets perm to use microphone

---
+++ src/lib/AudioStream.js
@@ -0,0 +1,12 @@
+function getLocalStream() {
+  navigator.mediaDevices
+    .getUserMedia({ video: false, audio: true })
+    .then((stream) => {
+      window.localStream = stream; // A
+      window.localAudio.srcObject = stream; // B
+      window.localAudio.autoplay = true; // C
+    })
+    .catch((err) => {
+      console.error(`you got an error: ${err}`);
+    });
+}
