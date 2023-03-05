5b3c917b95056edfc2a8e35fc2c14c3ddafd9732
andrew <asc269@cornell.edu>
2023-03-05 00:54:00 -0500

    Implement getAudioStream()

--- src/lib/AudioStream.js
+++ src/lib/AudioStream.js
@@ -1,12 +1,34 @@
-function getLocalStream() {
-  navigator.mediaDevices
-    .getUserMedia({ video: false, audio: true })
-    .then((stream) => {
-      window.localStream = stream; // A
-      window.localAudio.srcObject = stream; // B
-      window.localAudio.autoplay = true; // C
-    })
-    .catch((err) => {
-      console.error(`you got an error: ${err}`);
-    });
-}
+const getAudioStream = () => {
+  return new Promise((resolve, reject) => {
+    navigator.mediaDevices
+      .getUserMedia({ video: false, audio: true })
+      .then((stream) => {
+        const audioContext = new AudioContext();
+        const sourceNode = audioContext.createMediaStreamSource(stream);
+        const scriptProcessorNode = audioContext.createScriptProcessor(
+          4096,
+          1,
+          1
+        );
+        const pcmData = [];
+
+        scriptProcessorNode.addEventListener("audioprocess", (event) => {
+          const inputBuffer = event.inputBuffer;
+          const channelData = inputBuffer.getChannelData(0);
+          pcmData.push(...channelData);
+        });
+
+        sourceNode.connect(scriptProcessorNode);
+        scriptProcessorNode.connect(audioContext.destination);
+
+        stream.onended = () => {
+          resolve(pcmData);
+        };
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+export default getAudioStream;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
+import React, { useState, useEffect } from "react";
+
+import getAudioStream from "@/lib/AudioStream";
 export default function Home() {
+  useEffect(() => {
+    getAudioStream();
+  }, []);
+
   return <div></div>;
 }
