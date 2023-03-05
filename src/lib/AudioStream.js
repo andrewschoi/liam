8a8e50b80a023c290da5d6fe9009623205bd5581
alexkgiang <104709844+alexkgiang@users.noreply.github.com>
2023-03-05 01:43:27 -0500

    Comment AudioStream

--- src/lib/AudioStream.js
+++ src/lib/AudioStream.js
@@ -1,17 +1,18 @@
-const getAudioStream = () => {
+const getAudioStream = () => {  //promise of PCM audio data array
   return new Promise((resolve, reject) => {
     navigator.mediaDevices
-      .getUserMedia({ video: false, audio: true })
+      .getUserMedia({ video: false, audio: true })  //request user microphone
       .then((stream) => {
         const audioContext = new AudioContext();
         const sourceNode = audioContext.createMediaStreamSource(stream);
         const scriptProcessorNode = audioContext.createScriptProcessor(
-          4096,
+          4096,  //process audio data in real time
           1,
           1
         );
-        const pcmData = [];
+        const pcmData = [];  //for PCM audio data
 
+        //listen for audio events, extract from event object
         scriptProcessorNode.addEventListener("audioprocess", (event) => {
           const inputBuffer = event.inputBuffer;
           const channelData = inputBuffer.getChannelData(0);
