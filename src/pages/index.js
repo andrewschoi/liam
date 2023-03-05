b8f0d4dc45ce2eccd5f3dc4c79a15a611fd614da
andrew <asc269@cornell.edu>
2023-03-05 18:11:24 -0500

    Delete AudioStream

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,5 @@
 import React, { useState, useEffect } from "react";
-import getAudioStream from "@/lib/AudioStream";
 
 export default function Home() {
-  const [audioBuffer, setAudioBuffer] = useState([]);
-
-  const handleAudioChange = (pcm) => {
-    setAudioBuffer(pcm);
-  };
-
-  useEffect(() => {
-    getAudioStream(handleAudioChange);
-  }, []);
-
-  return <div>{audioBuffer}</div>;
+  return <div></div>;
 }
