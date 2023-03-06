05a63765fd45721beafee31f841f5bab13a412cd
andrew <asc269@cornell.edu>
2023-03-05 22:26:19 -0500

    Add input and submit fields to Home

--- src/lib/Processing.js
+++ src/lib/Processing.js
@@ -31,4 +31,5 @@ export const delimitWords = (wordList) => {};
  * @param {string list} wordList
  */
 export const truncateWords = (wordList, limit) => {};
+
 export default removeNestedWords;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,35 @@ import Transcription from "@/lib/Transcription";
 
 export default function Home() {
   const [transcript, setTranscript] = useState([]);
-  useEffect(() => {
-    const transcriptionClient = new Transcription();
-    transcriptionClient.startRecording("en-US", (res) =>
-      setTranscript((prev) => [...prev, res])
-    );
+  const [question, setQuestion] = useState("");
 
-    return () => transcriptionClient.stopRecording();
-  }, []);
+  // useEffect(() => {
+  //   const transcriptionClient = new Transcription();
+  //   transcriptionClient.startRecording("en-US", (res) =>
+  //     setTranscript((prev) => [...prev, res])
+  //   );
+
+  //   return () => transcriptionClient.stopRecording();
+  // }, []);
+
+  const handleQuestionChange = (e) => {
+    setQuestion(e.target.value);
+  };
+
+  const handleSubmit = () => {};
   return (
     <div>
       <div className="transcript-container">
         <h1 className="transcript-header">Transcript</h1>
         <p className="transcript-body">{transcript}</p>
+      </div>
+
+      <div className="question-container">
+        <input
+          placeholder="ask questions..."
+          onChange={(e) => handleQuestionChange(e)}
+        />
+        <button>Ask Question</button>
       </div>
     </div>
   );
