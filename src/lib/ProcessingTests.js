af6ac0439a6333645e1e276fda57c1130d209666
alexkgiang <104709844+alexkgiang@users.noreply.github.com>
2023-03-05 22:20:39 -0500

    Add ProcessingTests

--- src/lib/ProcessingTests.js
+++ src/lib/ProcessingTests.js
@@ -1,1 +1,45 @@
 import removeNestedWords, { delimitWords } from "./Processing";
+
+describe("removeNestedWords", () => {
+  test("empty list", () => {
+    const wordList = [];
+    const ans = removeNestedWords(wordList);
+    expect(ans).toEqual([]);
+  })
+
+  test("empty string list", () => {
+    const wordList = [""];
+    const ans = removeNestedWords(wordList);
+    expect(ans).toEqual([]);
+  })
+
+  test("spaces string list", () => {
+    const wordList = [" ", "", " "];
+    const ans = removeNestedWords(wordList);
+    expect(ans).toEqual([]);
+  })
+
+  test("one string list", () => {
+    const wordList = [" ", "cheese", " "];
+    const ans = removeNestedWords(wordList);
+    expect(ans).toEqual(["cheese"]);
+  })
+
+  test("flatten word list", () => {
+    const wordList = ["computer", "vision", "is hard"];
+    const ans = removeNestedWords(wordList);
+    expect(ans).toEqual(["computer", "vision", "is", "hard"]);
+  })
+
+  test("flatten word list", () => {
+    const wordList = ["wings", "", "over ithaca", " "];
+    const ans = removeNestedWords(wordList);
+    expect(ans).toEqual(["wings", "over", "ithaca"]);
+  })
+
+  test("multiple word list", () => {
+    const wordList = ["cheese mouse or big mouse"];
+    const ans = removeNestedWords(wordList);
+    expect(ans).toEqual(["cheese", "mouse", "or", "big", "mouse"]);
+  })
+})
