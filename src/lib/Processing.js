39d19108061c956ed2e59b09f12b13e593f6167b
andrew <asc269@cornell.edu>
2023-03-05 21:25:09 -0500

    Add specifications for removeNestedWords and delimitWords

---
+++ src/lib/Processing.js
@@ -0,0 +1,16 @@
+/**
+ * Returns a "flattened" list, list whose elements contain no spaces and
+ * "nested" elements with spaces are separated into distinct elements,
+ * preserving order
+ * @param {string list} wordList
+ */
+
+const removeNestedWords = (wordList) => {};
+
+/**
+ * Returns a modified list st. every word that ends with a non-alphanumeric
+ * character is succeeded by element char
+ * @param {string list} wordList
+ * @param {string} char
+ */
+const delimitWords = (wordList, char) => {};
